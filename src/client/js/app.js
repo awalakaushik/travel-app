79a854b96cce9ba51e6a4a4111c63c55c16af257
awalakaushik <kawala@uh.edu>
2020-05-20 16:48:56 -0500

    add functionality for submit and save

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,41 +5,97 @@ import {
     fetchWeatherForecast,
     fetchPhoto,
     fetchCountryInfo,
+    getCountdown,
     FORECAST
 } from "./travel";
 
-async function handleSubmit(e) {
+// global variables
 
-    let trip = {};
+const tripInfo = {};
+
+let savedTrips = [];
+
+// Helper functions
+function updateUI(savedTrips) {
+    
+    if (!savedTrips) {
+        return;
+    }
+}
+
+function updateSearchView(tripInfo) {
+    const tripCityElement = document.getElementById('trip-city');
+    const weatherElement = document.getElementById('weather');
+    const tripCountdownElement = document.getElementById('trip-countdown');
+
+    tripCityElement.innerHTML = tripInfo.city;
+    weatherElement.innerHTML = tripInfo.weatherForecast.temperature;
+    tripCountdownElement.innerHTML = tripInfo.countdown;
+}
+
+async function handleSubmit(e) {
 
     e.preventDefault();
 
     // fetch trip details
-    trip.city = getTripCity();
-    trip.startDate = getTripStartDate();
-
+    tripInfo.city = getTripCity();
+    tripInfo.startDate = getTripStartDate();
+    
+    if (!tripInfo.city || !tripInfo.startDate) {
+        throw new Error("City and Start Date are required!");
+    }
+    
+    // get countdown
+    tripInfo.countdown = getCountdown(tripInfo.startDate);
+    
     // fetch the location information from geonames
-    trip.location = await fetchLocation(trip.city);
+    tripInfo.location = await fetchLocation(tripInfo.city);
+
+    if ( tripInfo.location === null) {
+        throw new Error("Error fetching location details... Try again later!");
+    }
 
     // fetch weather forecast for trip from Weatherbit
-    trip.weatherForecast = await fetchWeatherForecast(trip.location.latitude, trip.location.longitude);
+    tripInfo.weatherForecast = await fetchWeatherForecast(tripInfo.location.latitude, tripInfo.location.longitude);
 
     // fetch the location photo from pixabay
-    trip.photoUrl = await fetchPhoto(trip.city, trip.location.country);
+    tripInfo.photoUrl = await fetchPhoto(tripInfo.city, tripInfo.location.countryName);
 
     // fetch the country information from restcountries
-    trip.location.info = await fetchCountryInfo(trip.location.country);
+    tripInfo.location.country = await fetchCountryInfo(tripInfo.location.countryName);
 
-    console.log(trip);
-
-    // update UI
-    updateUI();
+    console.log(tripInfo);
+    updateSearchView(tripInfo);
 }
 
-function updateUI() {
+// Save trip information
+const handleSave = async (e) => {
+    e.preventDefault();
+  
+    try {
+        const response = await fetch('http://localhost:8081/trip/save', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ trip: tripInfo })
+        });
     
-}
+        if (!response.ok) {
+            return null;
+        }
+        
+        savedTrips = await response.json();
+        
+        // TODO: update UI
+        //updateUI(savedTrips);
+        console.log(savedTrips);
+        return savedTrips;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 export {
-    handleSubmit
+    handleSubmit,
+    handleSave
 }
