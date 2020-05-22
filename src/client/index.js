bb83aa3378c5710cb08fb4febc5cdcf6edea9b95
awalakaushik <kawala@uh.edu>
2020-05-22 16:25:44 -0500

    remove form listener

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,4 @@ import { handleSubmit, handleSave } from "./js/app";
 // import styles
 import "./styles/style.scss";
 
-// Add event listeners
-
-document.getElementById("btnSubmit").addEventListener('click', handleSubmit);
 document.getElementById('btnSave').addEventListener('click', handleSave);
